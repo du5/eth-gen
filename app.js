7b665fb21228ff6f78bf3bf9d4837b22b5bc286d
孔雀 <54979375+du5@users.noreply.github.com>
2021-11-16 01:21:29 +0800

    优化异常处理

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ const Web3 = require("web3");
 let eth = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545/")).eth;
 const start = async function () {
     do {
-        let account = eth.accounts.create()
-        await eth.getBalance(account.address).then(
-            (data) => {
-                if (data != "0") {
-                    console.log("%s: %s", account, data);
+        try {
+            let account = eth.accounts.create()
+            await eth.getBalance(account.address).then(
+                (data) => {
+                    if (data != "0") {
+                        console.log("%s: %s", account, data);
+                    }
                 }
-            }
-        );
+            );
+        } catch (error) {
+            console.error(error.message);
+        }
     } while (true);
 }
 start();
